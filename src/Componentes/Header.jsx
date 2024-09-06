2cad3bf88e8f2fa05f34fac604f5cf46502dc407
prog2 <yampajoel@gmail.com>
2024-09-06 11:08:50 -0300

    implementado los componentes placeholder footer y header

---
+++ src/Componentes/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    
+    <div className="bg-gray-500">Header</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+import Header from './Componentes/Header.jsx'
+import Footer from './Componentes/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header/>
     <App />
+    <Footer/>
   </StrictMode>,
 )
